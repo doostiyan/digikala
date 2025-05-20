d842e14b67e20fd39f00e59aabb3b0ffdc6abb1c
Amir <amirh9476@gmail.com>
2025-05-20 17:42:09 +0330

    Add models order and order item

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from shop.models import Product
 # Create your models here.
 
 class ShopingAddress(models.Model):
@@ -22,4 +23,26 @@ class ShopingAddress(models.Model):
 
     def __str__(self):
         return f"{self.shoping_full_name}"
-    
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField()
+    shopping_address = models.TextField()
+    amount_paid = models.DecimalField(max_digits=12, decimal_places=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order{self.id}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=12, decimal_places=0)
+
+    def __str__(self):
+        return f"Order Item{self.id}"
