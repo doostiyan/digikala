912a8f21949f993d15ef2b970c39d994e345e0cf
Amir <amirh9476@gmail.com>
2025-07-11 14:19:51 +0330

    add checkout.html

--- payment/views.py
+++ payment/views.py
@@ -9,15 +9,16 @@ class PaymentSuccessView(TemplateView):
     template_name = 'payment/payment_success.html'
 
 class CheckoutView(views.View):
-    def get(request, ):
+    def get(self, request, ):
         cart = Cart(request)
         cart_products = cart.get_prods()
         quantities = cart.get_quants()
         total = cart.get_total()
         if request.user.is_authenticated:
             shopping_user = ShoppingAddress.objects.get(user__id=request.user.id)
-            shopping_form = ShoppingForm(request.POST or None, isinstance=shopping_user)
-            return render(request, 'payment/checkout.html', {'cart_product': cart_products, 'quantities': quantities, 'total': total, 'shopping_form': shopping_form})
+            shopping_form = ShoppingForm(request.POST or None, instance=shopping_user)
+            return render(request, 'payment/checkout.html', {'cart_products': cart_products, 'quantities': quantities, 'total': total, 'shopping_form': shopping_form})
         else:
             shopping_form = ShoppingForm(request.POST or None)
-            return render(request, 'payment/checkout.html', {'cart_product': cart_products, 'quantities': quantities, 'total': total, 'shopping_form': shopping_form})
+            return render(request, 'payment/checkout.html', {'cart_products': cart_products, 'quantities': quantities, 'total': total, 'shopping_form': shopping_form})
+
